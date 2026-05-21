e5834073277a1942ada60563db15e14c7245d679
Trogoz <trogoz@gmail.com>
2026-05-22 00:19:19 +0200

    Update and rename wkspace.bash to wkspacenotifier.bash

--- scripts/wkspacenotifier.bash
+++ scripts/wkspacenotifier.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
+#read current workspace name, add +1
 CURRENT_WORKSPACE=$(($(xprop -root -notype _NET_CURRENT_DESKTOP|sed 's/_NET_CURRENT_DESKTOP = //g')+1))
 while true
 do
+	#check every second
 	sleep 1
 	NEW_WORKSPACE=$(($(xprop -root -notype _NET_CURRENT_DESKTOP|sed 's/_NET_CURRENT_DESKTOP = //g')+1))
+	#if there is a change, save the current value and notify the number 
 	if [ $CURRENT_WORKSPACE -ne $NEW_WORKSPACE ]; then
 		CURRENT_WORKSPACE=$NEW_WORKSPACE
 		notify-send -t 2000 $CURRENT_WORKSPACE
