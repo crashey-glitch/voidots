259b576f649e4d8e1b60412b09291324a72e3b9d
Trogoz <trogoz@gmail.com>
2026-05-22 00:17:00 +0200

    Update lux.bash

--- scripts/lux.bash
+++ scripts/lux.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+#requires omen-light (duh!)
+#check paths!
 /home/trogoz/scripts/omen-light/omen_light 1 on static 0 0 0
 /home/trogoz/scripts/omen-light/omen_light 4 on static 0 255 0
 /home/trogoz/scripts/omen-light/omen_light 2 on static 0 0 0
